fbfae5010feddb2f5a7c3877a0c18ca9a8789ad8
karis <karis@192.168.1.37>
2024-03-21 21:18:19 +0530

    Assertion (Hard And Soft Assert)

---
+++ com.AutomationFramework/src/test/java/TestNGExecution/ActiTimeHardAssert.java
@@ -0,0 +1,39 @@
+package TestNGExecution;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ActiTimeHardAssert {
+	@Test
+	public void login() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("http://127.0.0.1/login.do");
+
+		String expectedLoginPageTitle = "actiTIME - Login";
+
+		// For critical Test Cases
+		Assert.assertEquals(driver.getTitle(), expectedLoginPageTitle, "Login page is not displayed");
+
+		WebElement usn = driver.findElement(By.name("username"));
+		Assert.assertEquals(usn.isDisplayed(), true, "Username TextBox is not dispalyed");
+		usn.sendKeys("admin");
+
+		WebElement pwd = driver.findElement(By.name("pwd"));
+		Assert.assertEquals(pwd.isDisplayed(), true, "Password Textbox is not displayed");
+		pwd.sendKeys("manager");
+
+		WebElement loginButton = driver.findElement(By.id("loginButton"));
+		Assert.assertEquals(loginButton.isDisplayed(), true, "Login Button is not displayed");
+		loginButton.click();
+
+	}
+}
